9cc49eb910394503044ad9e5ad816daaa33dd837
Oshoaro Favour <oshoarofavour@gmail.com>
2024-02-10 14:38:37 +0100

    installed cors package

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const express = require('express')
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello Favour, welcome to express.js')
+})
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
