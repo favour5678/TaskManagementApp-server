794acec0ffb58ca50c26cacd74278aadbd61b267
Oshoaro Favour <oshoarofavour@gmail.com>
2024-02-21 08:48:25 +0100

    called the task route in the index.js file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const taskRoute = require('./routes/taskRoute')
 
 const app = express()
 const port = 4000;
@@ -19,6 +20,6 @@ db.on("error", function (err) {
     console.error("Error connecting to MongoDB:", err);
 });
 
-// app.use('/tasks', taskRou)
+app.use('/tasks', taskRoute)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
