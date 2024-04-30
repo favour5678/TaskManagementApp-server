496e8bd4c7bb7d12bef8097ace8ebaf5c0ff3f7f
Oshoaro Favour <oshoarofavour@gmail.com>
2024-04-30 14:52:51 +0100

    added a new route for updating tasks

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const TaskModel = require('../models/Task');
 
+// Get all tasks
 router.get('/', async (req, res) => {
     try {
       const tasks = await TaskModel.find();
@@ -11,6 +12,7 @@ router.get('/', async (req, res) => {
     }
   })
   
+// Create a new task
 router.post('/', async(req, res) => {
     const { task } = req.body
     try {
@@ -20,6 +22,22 @@ router.post('/', async(req, res) => {
     } catch (error) {
       res.status(500).json({ error: 'Internal Server Error'})
     }
+})
+
+// Update a task
+router.put('/:id', async (req, res) => {
+  const taskId = req.params.id;
+  const { task } = req.body;
+
+  try { 
+    const updatedTask = await TaskModel.findByIdAndUpdate( taskId, {content: task}, {new: true} )
+    if(!updatedTask) {
+      return res.status(400).json({ error: 'Task not found' })
+    }
+    res.json({ success: true, task: updatedTask })
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error'})
+  }
 })
 
 module.exports = router;
